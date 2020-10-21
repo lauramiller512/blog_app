92fe06097676d00bd0ca8877e319edc4724f6e7c
Laura Miller <laura.miller03@bbc.co.uk>
2020-10-21 13:50:14 +0000

    Fixed template so article text now appears

--- app.py
+++ app.py
@@ -7,26 +7,37 @@ import re
 import logging
 from functools import wraps
 from time import time
-import psycopg2
 
 from models.user import User
 from models.article import Article
-from contextlib import contextmanager
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, mapper, relationship
+import sqlalchemy
 
 app = Flask(__name__)
 
 EMAIL_PATTERN = r"^([a-z]+)\.([a-z]+)\d*@bbc\.co\.uk$"
 
-engine = create_engine('postgresql://blog-app-user:lantern_rouge@127.0.0.1:8080/blog-app-db')
-#                     ('postgresql://username:password@host:port/database')
-Session = sessionmaker(bind=engine)
-session = Session()
+engine = create_engine(
+    'postgresql://blog-app-user:lantern_rouge@127.0.0.1:5432/blog-app-db',
+    pool_size=20
+)
 
-def get_database_connection():
-    conn = psycopg2.connect("dbname='blog-app-db' user='blog-app-user' host='127.0.0.1' password='lantern_rouge'")
-    return conn
+session_factory = sessionmaker(bind=engine)
+
+
+def map_tables(engine):
+    meta = sqlalchemy.MetaData()
+    meta.reflect(bind=engine)
+    t = meta.tables
+    mapper(User, t["authors"])
+    mapper(Article, t["articles"], properties={
+        "created_by": relationship(User)
+        })
+
+
+map_tables(engine)
+
 
 def measure_time(f):
     @wraps(f)
@@ -38,31 +49,12 @@ def measure_time(f):
         return result
     return decorated_function
 
-def db_connection():
-    connection = get_database_connection()
-    cur = connection.cursor()
-    return cur  
-
-def insert_article(title, text, author_id):
-    with get_database_connection() as connection:
-        with connection.cursor() as cur:
-            cur.execute(
-                """INSERT into articles(title, txt, author_id) 
-                values (%s, %s, %s)""", 
-                (title, text, author_id)
-                )
-
-@contextmanager
-def run_query(*query_args):
-    with get_database_connection() as connection:
-        with connection.cursor() as cur:
-            cur.execute(*query_args)
-            yield cur
 
 @app.errorhandler(BadRequest)
 def bad_request(bad_request):
     logging.warn(str(bad_request))
     return Response(json.dumps({"error": str(bad_request)})), 400
+
 
 @app.errorhandler(NotFound)
 def not_found(error):
@@ -71,83 +63,85 @@ def not_found(error):
     else:
         return render_template("not_found.html"), 404
 
+
 @app.route("/json")
 def hello_world():
     return {
         "message": "hello world"
     }
 
+
 @app.route("/json/articles", methods=["GET", "POST"])
 @measure_time
 def get_articles():
     if request.method == "GET":
-        articles = retrieve_articles()   
-        return Response(json.dumps(articles), mimetype="application/json")
+        session = session_factory()
+        articles = session.query(Article).all()
+        return Response(json.dumps(
+            [{"title": i.title} for i in articles]
+        ), mimetype="application/json")
     else:
         data = request.json
         try:
-            articles[max(articles.keys()) + 1] = Article(data["text"], data["title"], data["created_by"])
+            articles[max(articles.keys()) + 1] = Article(
+                data["text"],
+                data["title"],
+                data["created_by"]
+            )
         except KeyError:
             raise BadRequest("Invalid data provided")
         return (
-            Response(json.dumps({"message": "Okay"}), mimetype="application/json"),
+            Response(json.dumps(
+                {"message": "Okay"}), mimetype="application/json"),
             201
             )
 
 
-@app.route("/json/articles/<int:article_id>", methods=["GET", "DELETE"])
+@app.route("/json/articles/<uuid:article_id>", methods=["GET", "DELETE"])
 @measure_time
 def get_article(article_id):
+    session = session_factory()
+    article = session.query(Article).filter_by(id=str(article_id)).first()
+    if article is None:
+        raise NotFound("This article cannot be found")
     if request.method == "GET":
-        try:
-            article = articles[article_id]
-            new_dict = {
-                "title": article.title,
-                "text": article.text,
-                "author": article.created_by.username
-            }
-        except KeyError:
-            raise NotFound("This article cannot be found")
-
+        new_dict = {
+            "title": article.title,
+            "text": article.txt,
+            "created_by": article.created_by.username
+        }
         return Response(json.dumps(new_dict), mimetype="application/json")
     else:
-        try:
-            del articles[article_id]
-            return "", 204
-        except KeyError:
-            raise NotFound("Article you're trying to delete does not exist")
+        session.delete(article)
+        session.commit()
+        return "", 204
 
 
 @app.route("/articles")
 @measure_time
 def get_articles_html():
-    with run_query("""SELECT id, title from articles""") as cur:
-        articles = cur.fetchall()
+    session = session_factory()
+    articles = session.query(Article).all()
     return render_template("articles.html", articles=articles)
+
 
 @app.route("/articles/<uuid:article_id>")
 @measure_time
 def get_article_id_html(article_id):
-    with run_query("""SELECT title, txt, author_id from articles where id=%s""", (str(article_id), )) as cursor:
-        article = cursor.fetchone()
-    if article is None:
-        raise NotFound
-    logging.warn(article)
-    with run_query("""SELECT username, firstname, lastname from authors where id=%s""", (article[2], )) as cursor:
-        user = cursor.fetchone()
-    user = User(*user)
-    article = Article(article[1], article[0], user)
+    session = session_factory()
+    article = session.query(Article).filter_by(id=str(article_id)).first()
     logging.warn(article)
     try:
         return render_template("article_id.html", article=article)
     except KeyError:
         raise NotFound
 
+
 @app.route("/articles/create", methods=["GET", "POST"])
 @measure_time
 def create_article():
-    connection = get_database_connection()
-    cur = connection.cursor()
+    session = session_factory()
+
     if request.method == "GET":
         return render_template("create_article.html")
     else:
@@ -160,47 +154,21 @@ def create_article():
         m = re.match(EMAIL_PATTERN, email)
         if m is None:
             raise BadRequest("Invalid email address")
-        fname, lname = m.groups()        
+        fname, lname = m.groups()
         username = "_".join([fname, lname])
         user = User(username, fname, lname)
         # two insert statements, db will throw error if username not unique
-
-        logging.warn(2)
-
+        session.add(user)
         try:
-            run_query(
-                """INSERT into authors(firstname, lastname, username) 
-                values (%s, %s, %s)""",
-                (fname, lname, username)
-                )
+            session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            session.rollback()
+            user = session.query(User).filter_by(username=username).first()
 
-            with run_query("""SELECT id from authors where username=%s""", (username, )) as cursor:
-                author = cursor.fetchone()
-        except psycopg2.errors.UniqueViolation:
-            with run_query("""SELECT id from authors where username=%s""", (username, )) as cursor:
-                author = cursor.fetchone()
+        article = Article(text, title, user)
+        session.add(article)
+        session.commit()
 
-        logging.warn(4)
-
-        try:
-            run_query(
-                """INSERT into articles(title, txt, author_id) values (%s, %s, %s)""", 
-                (title, text, author[0]))
-            logging.warn("article written")
-
-        except Exception as e:
-            logging.warn(e)
-        
-        logging.warn(5)
-
-        with run_query("""SELECT id, title from articles""") as cur:
-            articles = cur.fetchall()
+        articles = session.query(Article).all()
 
         return render_template("/articles.html", articles=articles)
-    
-        
-
-#list of articles should be linked to specific article, displaying title plus text
-#endpoint for article_id to also have html 404 error message
-    
-
--- models/article.py
+++ models/article.py
@@ -3,4 +3,5 @@ class Article():
     def __init__(self, text, title, created_by):
         self.text = text
         self.title = title
-        self.created_by = created_by #should be a user object
+        self.created_by = created_by
+        
